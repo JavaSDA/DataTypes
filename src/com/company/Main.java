1de9b1c8a4a47bb8ee1f0c102ec87494d80d5509
Uche Lawal <thatboywayne95@gmail.com>
2021-06-18 18:41:00 +0100

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	// Integers
+    	// These are the variables i declared
+	    int firstNumber;
+	    int secondNumber;
+	    // This is where I initialized them
+	    firstNumber = 5;
+	    secondNumber = 6;
+
+	    int sum = firstNumber + secondNumber;
+//	    System.out.println(sum);
+
+		// Floats
+	    float firstFloat = 0.5f;
+	    float secondFloat = 2.3f;
+
+	    float floatSum = firstFloat + secondFloat;
+//	    System.out.println(floatSum);
+
+		// Doubles
+	    double firstDouble = 0.7;
+	    double secondDouble = 2.4;
+	    double doubleSum = firstDouble + secondDouble;
+
+//	    System.out.println(doubleSum);
+
+		String firstString = "Chips";
+		String secondString = "Chicken";
+
+		System.out.println("I love " + secondString + " and " + firstString);
+
+		
+
+    }
+}
